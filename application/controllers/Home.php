a7f47e748fe73ff756fef78094c0eb585d22d124
MOCH-PC\Muhammad Khotib <mkhotib20@gmail.com>
2017-10-10 03:39:14 +0700

    Edit autoload, routes dan controller peminjaman, prosespeminjaman

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,6 +7,92 @@ class home extends CI_Controller {
 		$diff =  $tanggal_lahir - $sekarang;
 		$hasil = floor($diff / (60 * 60 * 24))+1 ;
 	}
+	public function peminjaman(){
+		$data=$this->Models->read('peminjaman','','')->result_array();
+		$kirim['tampil']  = $data;
+		$this->load->view('header');
+		$this->load->view('peminjaman', $kirim);
+		$this->load->view('footer');
+	}
+	public function inputPeminjaman(){
+		$kirim['kode'] = time();
+		$dataM=$this->Models->read('mobil','','')->result_array();
+		$dataS=$this->Models->read('supir','','')->result_array();
+		$kirim['tampilM']  = $dataM;
+		$kirim['tampilS']  = $dataS;
+		$this->load->view('header');
+		$this->load->view('input-pinjam', $kirim);
+		$this->load->view('footer');
+	}
+	public function prosesPinjam(){
+		$kodePeminjaman = $this->input->post('kodePeminjaman');
+		$namaPeminjam = $this->input->post('namaPeminjam');
+		$alamatPeminjam = $this->input->post('alamatPeminjam');
+		$ktpPeminjam = $this->input->post('ktpPeminjam');
+		$hpPeminjam = $this->input->post('hpPeminjam');
+		$uangMuka = $this->input->post('uangMuka');
+		$kondisiKendaraan = $this->input->post('kondisiKendaraan');
+		$tglBalik = $this->input->post('tglBalik');
+
+		$tglKembali = strtotime($tglBalik);
+		$tglPinjam    = time(); 
+		$diff =  $tglKembali - $tglPinjam;
+		$totalHari = floor($diff / (60 * 60 * 24))+1;
+
+		$mobil = $this->input->post('mobil');
+		
+		$supir = $this->input->post('supir');
+		if ($supir=='') {
+			$ktp_supir='0';
+			$biayaSupir = 0;
+		}
+		else{
+			$getSupir = $this->Models->readWH('supir', 'nama_supir', $supir)->result_array();
+			foreach ($getSupir as $g) {
+				$ktp_supir = $g['no_ktp'];
+			}
+			$biayaSupir = 100000;
+		}
+		$variable = $this->Models->readWH('mobil', 'merk', $mobil)->result_array();
+		foreach ($variable as $v) {
+			$tarif  = $v['harga'];
+			$no_pol  = $v['no_polisi'];
+		}
+		$totalBiaya = ($totalHari * $tarif) +$biayaSupir;	
+		
+
+		$dataPeminjam = array(
+			'no_ktp_peminjam' => $ktpPeminjam,
+			'nama_peminjam' => $namaPeminjam,
+			'no_hp_peminjam' => $hpPeminjam,
+			'alamat_peminjam' => $alamatPeminjam,
+		);
+
+		$dataPeminjaman = array(
+			'kode_pinjam' => $kodePeminjaman, 
+			'no_pol' => $no_pol, 
+			'no_ktp_spr' => $ktp_supir, 
+			'no_ktp_pjm' => $ktpPeminjam, 
+			'tgl_pinjam' => date('Y-m-d'), 
+			'tgl_kembali' => $tglBalik, 
+			'biaya_total' => $totalBiaya, 
+			'status' => 0, 
+			'uang_muka' => $uangMuka, 
+			'kondisi' => $kondisiKendaraan, 
+		);
+		if ($this->Models->input('peminjam', $dataPeminjam)) {
+			if ($this->Models->input('peminjaman', $dataPeminjaman)) {
+				echo 'berhasil';
+				redirect(base_url('home/peminjaman'));
+			}
+			else{
+				echo 'gagal peminjaman';
+			}
+		}
+		else{
+			echo 'gaga;';
+		}
+	}
 	public function index(){
 		
 		$this->load->view('header');
@@ -62,7 +148,7 @@ class home extends CI_Controller {
 			}
 		}
 		else{
-        }
+        }}
     }
 
 			

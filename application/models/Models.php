1fdc9ee89ed4623e787c9c8ce926a153cfff34a0
MOCH-PC\Muhammad Khotib <mkhotib20@gmail.com>
2017-10-17 21:20:31 +0700

    khotib models

--- application/models/Models.php
+++ application/models/Models.php
@@ -14,6 +14,12 @@ class models extends CI_Model
 	public function input($tabel, $data){
 		return $this->db->insert($tabel, $data);
 	}
+	public function update($tabel, $data, $where, $id){
+		return $this->db->update($tabel, $data, array($where => $id));
+	}
+	public function delete(){
+		
+	}
 	
 }
  ?>

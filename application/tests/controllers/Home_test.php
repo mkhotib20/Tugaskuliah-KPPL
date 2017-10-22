9316be6026d77f353f3bfc4f5fb049767601d9d9
YeyenSudiarti <yeyen.sudiarti@gmail.com>
2017-10-22 23:03:24 +0700

    testing home

---
+++ application/tests/controllers/Home_test.php
@@ -0,0 +1,22 @@
+<?php
+
+class Home_test extends TestCase
+{    
+        public function setUp(){
+            $this->resetInstance();
+            $this->CI->load->model('Models');
+            $this->obj1 = $this->CI->Models;
+        }
+    
+    public function test_view_home_sukses() {
+                $_SESSION['username'] = 'admin';
+                
+    		$output = $this->request('GET', 'Home/index');
+    		$this->assertContains('<title>Green Rent | Dashboard</title>', $output);
+    }
+    public function test_view_home_gagal() {
+    		$output = $this->request('GET', 'Home/index');
+    		$this->assertRedirect('login');
+    }
+}
+

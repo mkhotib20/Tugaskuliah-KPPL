47157c0f4c62d54b185d068fd537a643dcd9d2c8
Dyah Wiji <dyahwijiastuti@gmail.com>
2017-10-22 23:57:32 +0700

    test untuk login selesai

--- application/tests/controllers/login_test.php
+++ application/tests/controllers/login_test.php
@@ -1,10 +1,5 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 class login_test extends TestCase
 {
     protected $backupGlobalsBlacklist = array( '_SESSION' );
@@ -16,18 +11,84 @@ class login_test extends TestCase
             $this->obj1 = $this->CI->Models;
         }
     
-    public function test_view_login() {
-    		$output = $this->request('GET', 'Login/index');
-    		$this->assertContains('<title>Greent Rent | Log in</title>', $output);
-    }
+        public function test_view_login() {
+            $output = $this->request('GET', 'Login/index');
+            $this->assertContains('<title>Greent Rent | Log in</title>', $output);
+        }
 
-    public function test_login_masuk_berhasil() {
-        $this->request('POST', 'Login/prosesLogin',
-                            [
-                            'username' => 'admin',
-                            'password' => 'greenrental', 
-                            ]
-                            );
+        public function test_login_masuk_berhasil() {
+             $this->request('POST', 'Login/prosesLogin',
+                [
+                    'username' => 'admin',
+                    'password' => 'greenrental', 
+                ]);
             $this->assertEquals('admin', $_SESSION['username']);
-    }
+        }
+        
+        public function test_adminlogin_gagal1(){
+            $this->request('POST', 'Login/prosesLogin',
+                [
+                    'username' => 'admin',
+                    'password' => 'salah',
+                ]);
+            $this->assertRedirect('login');
+            $this->assertFalse( isset($_SESSION['username']) );
+        }
+        
+        public function test_adminlogin_gagal2(){
+            $this->request('POST', 'Login/prosesLogin',
+                [
+                    'username' => '',
+                    'password' => 'greenrental',
+                ]);
+            $this->assertRedirect('login');
+            $this->assertFalse( isset($_SESSION['username']) );
+        }
+        
+        public function test_adminlogin_gagal3(){
+            $this->request('POST', 'Login/prosesLogin',
+                [
+                    'username' => 'admin',
+                    'password' => '',
+                ]);
+            $this->assertRedirect('login');
+            $this->assertFalse( isset($_SESSION['username']) );
+        }
+        
+        public function test_adminlogin_gagal4(){
+            $this->request('POST', 'Login/prosesLogin',
+                [
+                    'username' => 'admin',
+                    'password' => 'Greenrental',
+                ]);
+            $this->assertRedirect('login');
+            $this->assertFalse( isset($_SESSION['username']) );
+        }
+        
+        public function test_adminlogin_gagal5(){
+            $this->request('POST', 'Login/prosesLogin',
+                [
+                    'username' => 'salahh',
+                    'password' => 'unmatch',
+                ]);
+            $this->assertRedirect('login');
+            $this->assertFalse( isset($_SESSION['username']) );
+        }
+        
+        public function test_adminlogin_gagal6(){
+            $this->request('POST', 'Login/prosesLogin',
+                [
+                    'username' => '',
+                    'password' => '',
+                ]);
+            $this->assertRedirect('login');
+            $this->assertFalse( isset($_SESSION['username']) );
+        }
+        
+        public function test_admin_logout(){
+            $_SESSION['nama'] = "admin";
+            $_SESSION['status'] = "greenrental";
+            $this->request('GET', 'Login/logout');
+            $this->request('GET', 'Login/index');
+        }
 }

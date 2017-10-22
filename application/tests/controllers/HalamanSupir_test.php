4c09977c9de425c47c12ce70a8acfbcc00718d1b
Dyah Wiji <dyahwijiastuti@gmail.com>
2017-10-23 00:34:48 +0700

    menambahkan test input supir berhasil dan gagal pada HalamanSupir_test

--- application/tests/controllers/HalamanSupir_test.php
+++ application/tests/controllers/HalamanSupir_test.php
@@ -18,5 +18,43 @@ class HalamanSupir_test extends TestCase
     	$output = $this->request('GET', 'HalamanSupir/supir');
     	$this->assertRedirect('login');
     }
+    
+    public function test_input_supir(){
+        $_SESSION['username'] = 'admin';
+        $filename = '1.jpg';
+	$filepath = APPPATH.  'fototest/' .$filename;
+	$files = [
+            'gambarSupir' => [
+            'name'     => $filename,
+            'type'     => 'image/jpg',
+            'tmp_name' => $filepath 
+            ],
+            ];
+	$this->request->setFiles($files);
+        $awal1 = $this->obj1->getCurrentRow('supir');
+	$this->request('POST', 'HalamanSupir/prosesSupir',[
+            'noKTP' => '735711',
+            'namaSupir' => 'Testing',
+            'noHP' => '089898988765',
+            'alamatSupir' => 'Testing',
+            'noSIM' => '678989'
+            ]);
+        $akhir1 = $this->obj1->getCurrentRow('supir');
+        $this->assertEquals($akhir1, $awal1+1);        
+    }
+    
+    public function test_input_supir_gagal(){
+        $_SESSION['username'] = 'admin';
+        $awal1 = $this->obj1->getCurrentRow('supir');
+	$this->request('POST', 'HalamanSupir/prosesSupir',[
+            'noKTP' => '',
+            'namaSupir' => '',
+            'noHP' => '',
+            'alamatSupir' => '',
+            'noSIM' => ''
+            ]);
+        $akhir1 = $this->obj1->getCurrentRow('supir');
+        $this->assertEquals($akhir1, $awal1);        
+    }
 }
 
